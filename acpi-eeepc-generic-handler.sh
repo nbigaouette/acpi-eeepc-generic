cf62bd4e2c007af6eacef58316c37922c44bfcfd
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-01-27 01:21:56 +0000

    Tabs to spaces

--- acpi-eeepc-generic-handler.sh
+++ acpi-eeepc-generic-handler.sh
@@ -10,7 +10,7 @@ su $XUSER --login -c "xhost +"
 
 SELECTION=$3
 if [ "$KEY_SHOW" = "1" ]; then
-  eeepc_notify "You pressed: \"$SELECTION\"" keyboard
+eeepc_notify "You pressed: \"$SELECTION\"" keyboard
 fi
 
 case "$1" in
@@ -19,85 +19,85 @@ case "$1" in
         eeepc_notify "Test event: ${cmds[@]}" keyboard
         execute_commands "${cmds[@]}"
     ;;
-	button/power)
-		case "$2" in
-			PWRF)
-			    eeepc_notify "Power button pressed" gnome-session-halt
-			    execute_commands "${COMMANDS_POWER_BUTTON[@]}"
-			;;
-			*)
-			    eeepc_notify "Button undefined: $2 $3 $4" keyboard
-			    logger "ACPI power undefined: $2 $3 $4"
-			;;
-		esac
-		;;
+    button/power)
+        case "$2" in
+            PWRF)
+                eeepc_notify "Power button pressed" gnome-session-halt
+                execute_commands "${COMMANDS_POWER_BUTTON[@]}"
+            ;;
+            *)
+                eeepc_notify "Button undefined: $2 $3 $4" keyboard
+                logger "ACPI power undefined: $2 $3 $4"
+            ;;
+        esac
+        ;;
 
-	button/sleep)
-		case "$2" in
-			SLPB)
-			    eeepc_notify "Sleep button pressed" gnome-session-suspend
-			    execute_commands "${COMMANDS_SLEEP[@]}"
-			;;
-			*)
-			    eeepc_notify "Button undefined: $2 $3 $4" keyboard
-			    logger "ACPI sleep undefined: $2 $3 $4"
-			;;
-		esac
-		;;
+    button/sleep)
+        case "$2" in
+            SLPB)
+                eeepc_notify "Sleep button pressed" gnome-session-suspend
+                execute_commands "${COMMANDS_SLEEP[@]}"
+            ;;
+            *)
+                eeepc_notify "Button undefined: $2 $3 $4" keyboard
+                logger "ACPI sleep undefined: $2 $3 $4"
+            ;;
+        esac
+        ;;
 
-	ac_adapter)
-		case "$2" in
-			AC0)
-				case "$4" in
-					$POWER_BAT) # AC off
-					    #echo 1500 > /proc/sys/vm/dirty_writeback_centisecs
+    ac_adapter)
+        case "$2" in
+            AC0)
+                case "$4" in
+                    $POWER_BAT) # AC off
+                        #echo 1500 > /proc/sys/vm/dirty_writeback_centisecs
                         execute_commands "${COMMANDS_AC_UNPLUGGED[@]}"
-            			#echo $BRIGHTNESS_BATTERY > /proc/acpi/asus/brn
-            			#xbacklight -set $BRIGHTNESS_BATTERY
-            			#if [ $WIRELESS_MODULE = "iwl3945" ]; then
-            			#echo $WIRELESS_POWERSAVE > /sys/bus/pci/drivers/iwl3945/0000:01:00.0/power_level;
-            			#fi
-					;;
-					$POWER_AC) # AC on
-					    #echo 500 > /proc/sys/vm/dirty_writeback_centisecs
-            			execute_commands "${COMMANDS_AC_PLUGGED[@]}"
-            			#echo $BRIGHTNESS_AC > /proc/acpi/asus/brn
-            			#xbacklight -set $BRIGHTNESS_AC
-            			#if [ $WIRELESS_MODULE = "iwl3945" ]; then
-            			#    echo 6 > /sys/bus/pci/drivers/iwl3945/0000:01:00.0/power_level;
-            			#fi
-					;;
-				esac
-				;;
-			*) logger "ACPI AC undefined: $2 $3 $4"
-			;;
-		esac
-		;;
+                        #echo $BRIGHTNESS_BATTERY > /proc/acpi/asus/brn
+                        #xbacklight -set $BRIGHTNESS_BATTERY
+                        #if [ $WIRELESS_MODULE = "iwl3945" ]; then
+                        #echo $WIRELESS_POWERSAVE > /sys/bus/pci/drivers/iwl3945/0000:01:00.0/power_level;
+                        #fi
+                    ;;
+                    $POWER_AC) # AC on
+                        #echo 500 > /proc/sys/vm/dirty_writeback_centisecs
+                        execute_commands "${COMMANDS_AC_PLUGGED[@]}"
+                        #echo $BRIGHTNESS_AC > /proc/acpi/asus/brn
+                        #xbacklight -set $BRIGHTNESS_AC
+                        #if [ $WIRELESS_MODULE = "iwl3945" ]; then
+                        #    echo 6 > /sys/bus/pci/drivers/iwl3945/0000:01:00.0/power_level;
+                        #fi
+                    ;;
+                esac
+                ;;
+            *) logger "ACPI AC undefined: $2 $3 $4"
+            ;;
+        esac
+        ;;
 
-	battery)
-		case "$2" in
-			BAT0)
-				case "$4" in
-					00000000) # Battery removed
-					;;
-					00000001) # Battery present
-					;;
-				esac
-				;;
-			*) logger "ACPI battery undefined: $2 $3 $4"
-			;;
-		esac
-		;;
+    battery)
+        case "$2" in
+            BAT0)
+                case "$4" in
+                    00000000) # Battery removed
+                    ;;
+                    00000001) # Battery present
+                    ;;
+                esac
+                ;;
+            *) logger "ACPI battery undefined: $2 $3 $4"
+            ;;
+        esac
+        ;;
 
-	button/lid)
+    button/lid)
         lidstate=$(cat /proc/acpi/button/lid/LID/state | awk '{print $2}')
-	    case "$lidstate" in
-		open)
-		    # screen on
-		    xset dpms force on
-		;;
-		closed)
-		    if [ "$COMMANDS_ON_LID_CLOSE" == "yes" ]; then
+        case "$lidstate" in
+        open)
+            # screen on
+            xset dpms force on
+        ;;
+        closed)
+            if [ "$COMMANDS_ON_LID_CLOSE" == "yes" ]; then
                 ac_state=$(cat /proc/acpi/ac_adapter/AC0/state | awk '{print $2}' )
                 case $ac_state in
                 on-line)
@@ -109,15 +109,15 @@ case "$1" in
                     execute_commands "${COMMANDS_LID_CLOSE_ON_BATTERY[@]}"
                 ;;
                 esac
-		    fi
-		;;
-		*)
-		    logger "Lid state undefined: $2 $3 $4"
-		;;
-	    esac
-	    ;;
-	hotkey)
-		case "$3" in
+            fi
+        ;;
+        *)
+            logger "Lid state undefined: $2 $3 $4"
+        ;;
+        esac
+        ;;
+    hotkey)
+        case "$3" in
             $EEEPC_BLANK) # Silver function button 1 (Blank)
                 logger "acpi-eeepc-generic-handler.sh (hotkey): Silver function button (Blank)"
                 execute_commands "${COMMANDS_BUTTON_BLANK[@]}"
@@ -224,15 +224,15 @@ case "$1" in
 # 			logger "Battery is critical, suspending"
 # 			$BATTERY_CRITICAL &
 # 		    ;;
-		    *) logger "ACPI hotkey undefined: $2 $3 $4"
-		    ;;
-		esac
-		;;
+            *) logger "ACPI hotkey undefined: $2 $3 $4"
+            ;;
+        esac
+        ;;
 
-	*)
-		eeepc_notify  "ACPI group/action undefined: $1 $2 $3 $4" keyboard 
-		logger "ACPI group/action undefined: $1 $2 $3 $4"
-		;;
+    *)
+        eeepc_notify  "ACPI group/action undefined: $1 $2 $3 $4" keyboard
+        logger "ACPI group/action undefined: $1 $2 $3 $4"
+        ;;
 esac
 
 #su $XUSER --login -c "xhost -"
