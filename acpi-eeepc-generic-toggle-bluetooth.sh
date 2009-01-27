a2e7d63f1c7b7eed0ce1a1d80d108de3c0339b4a
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-01-27 04:18:36 +0000

    New tag created from trunk -r278

--- acpi-eeepc-generic-toggle-bluetooth.sh
+++ acpi-eeepc-generic-toggle-bluetooth.sh
@@ -52,7 +52,7 @@ function debug_bluetooth() {
 }
 
 function radio_on {
-    eeepc_notify "Turning Bluetooth on..." gnome-dev-wavelan
+    eeepc_notify "Turning Bluetooth on..." bluetooth
 
     # Execute pre-up commands just once
     [ $1 -eq 1 ] && execute_commands "${COMMANDS_BLUETOOTH_PRE_UP[@]}"
@@ -73,12 +73,12 @@ function radio_on {
         # Execute post-up commands
         execute_commands "${COMMANDS_BLUETOOTH_POST_UP[@]}"
 
-        eeepc_notify "Bluetooth is now on" gnome-dev-wavelan
+        eeepc_notify "Bluetooth is now on" bluetooth
     ) || (
         eeepc_notify "Could not enable Bluetooth" stop
         # If module loading unsuccessful, try again
         if [ $1 -lt $WIFI_TOGGLE_MAX_TRY ]; then
-            eeepc_notify "Trying again in 2 second ($(($1+1)) / $WIFI_TOGGLE_MAX_TRY)" gnome-dev-wavelan
+            eeepc_notify "Trying again in 2 second ($(($1+1)) / $WIFI_TOGGLE_MAX_TRY)" bluetooth
             sleep 2
             radio_on $(($1+1))
         fi
@@ -86,7 +86,7 @@ function radio_on {
 }
 
 function radio_off {
-    eeepc_notify "Turning Bluetooth off..." gnome-dev-wavelan
+    eeepc_notify "Turning Bluetooth off..." bluetooth
 
     # Execute pre-down commands just once
     [ $1 -eq 1 ] && execute_commands "${COMMANDS_BLUETOOTH_PRE_DOWN[@]}"
@@ -109,12 +109,12 @@ function radio_off {
         # Execute post-down commands
         execute_commands "${COMMANDS_BLUETOOTH_POST_DOWN[@]}"
 
-        eeepc_notify "Bluetooth is now off" gnome-dev-wavelan
+        eeepc_notify "Bluetooth is now off" bluetooth
     ) || (
         # If module unloading unsuccessful, try again
         eeepc_notify "Could not disable Bluetooth" stop
         if [ $1 -lt $WIFI_TOGGLE_MAX_TRY ]; then
-            eeepc_notify "Trying again in 2 second ($(($1+1)) / $WIFI_TOGGLE_MAX_TRY)" gnome-dev-wavelan
+            eeepc_notify "Trying again in 2 second ($(($1+1)) / $WIFI_TOGGLE_MAX_TRY)" bluetooth
             sleep 2
             radio_off $(($1+1))
         fi
