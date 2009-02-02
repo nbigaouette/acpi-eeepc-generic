52beb4dac2a3d9713af37a1cd85ad578f50ff728
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-02-02 17:45:04 +0000

    Forgot to touch/rm the lock file

--- acpi-eeepc-generic-toggle-lock-suspend.sh
+++ acpi-eeepc-generic-toggle-lock-suspend.sh
@@ -2,13 +2,17 @@
 
 . /etc/acpi/eeepc/acpi-eeepc-generic-functions.sh
 
-if [ -e "${EEEPC_VAR}/power.lock" ]; then
+lock="${EEEPC_VAR}/power.lock"
+
+if [ -e "$lock" ]; then
     msg="Disabling suspend lock"
+    rm -f $lock
     logger "$msg"
     eeepc_notify "$msg" stop
     exit 0
 else
     msg="Enabling suspend lock"
+    touch $lock
     logger "$msg"
     eeepc_notify "$msg" gnome-session-suspend
     exit 0
