920fa19cc2182a8a07743fff352de39c97c07926
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-02-20 16:54:32 +0000

    Cleanup and comments for user autodetection. Thanx to http://code.google.com/p/acpi-eeepc-generic/issues/detail?id=8

--- acpi-eeepc-generic-functions.sh
+++ acpi-eeepc-generic-functions.sh
@@ -201,13 +201,37 @@ function brightness_find_direction() {
 # Get username
 if [ -S /tmp/.X11-unix/X0 ]; then
     export DISPLAY=:0
-    [ "x$user" == "x" ] && user=$(who | head -1 | awk '{print $1}')
-    # If autodetection fails, try another way...
+
+    # Detect logged in user by using "who"
     user=$(who | sed -n '/ (:0[\.0]*)$\| :0 /{s/ .*//p;q}')
+    # If there is a space in $user, autodetection failed, so clear it.
+    [ "x`echo $user | grep ' '`" != "x" ] && user=""
+
+    # If autodetection fails, try another way...
+    # Take first reported by "who"
+    #[ "x$user" == "x" ] && user=$(who | head -1 | awk '{print $1}')
+    # If there is a space in $user, autodetection failed, so clear it.
+    #[ "x`echo $user | grep ' '`" != "x" ] && user=""
+
+    # If autodetection fails, try another way...
+    # Take users reported by "who" and return uniq values
+    [ "x$user" == "x" ] && user=$(who | awk '{print $1}' | uniq)
+    # If there is a space in $user, autodetection failed, so clear it.
+    [ "x`echo $user | grep ' '`" != "x" ] && user=""
+
+    # If autodetection fails, try another way...
+    # Get user who executed startx. Might not work if a login
+    # manager (GDM,KDM,etc.) is used.
+    [ "x$user" == "x" ] && user=$(ps aux | grep 'xinit /home' | awk '{print $1}' | head -1)
+    # If there is a space in $user, autodetection failed, so clear it.
+    [ "x`echo $user | grep ' '`" != "x" ] && user=""
+
     # If autodetection fails, fallback to default user
     # set in /etc/conf.d/acpi-eeepc-generic.conf
     [ "x$user" == "x" ] && user=$XUSER
-    # If autodetection fails, try another way...
+
+    # As a last resort, check all processes and their owning user,
+    # filtering known users.
     [ "x$user" == "x" ] && user=$(ps aux | awk '{print ""$1""}' | \
         sort | uniq | \
         grep -v \
@@ -217,11 +241,16 @@ if [ -S /tmp/.X11-unix/X0 ]; then
         )
     # If there is a space in $user, autodetection failed, so clear it.
     [ "x`echo $user | grep ' '`" != "x" ] && user=""
-    # If user is empty, notify
+
+    # If user is _still_ empty, notify the user to set XUSER in
+    # configuration file.
     [ "x$user" == "x" ] && \
         eeepc_notify "User autodetection failed. Please edit \
 your configuration file (/etc/conf.d/acpi-eeepc-generic.conf) \
 and set XUSER variable to your username." stop 20000
+
+    # If user is detected correctly (not empty), set variables
+    # accordingly.
     if [ "x$user" != "x" ]; then
         home=$(getent passwd $user | cut -d: -f6)
         XAUTHORITY=$home/.Xauthority
