18f60034d86a6a976b11f02bda212e6f1dfa6a7b
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-03-22 23:08:32 +0000

    Adapted wifi toggle script to use generic functions

--- acpi-eeepc-generic-toggle-wifi.sh
+++ acpi-eeepc-generic-toggle-wifi.sh
@@ -5,184 +5,56 @@
 
 . /etc/acpi/eeepc/acpi-eeepc-generic-functions.sh
 
-# Find the right rfkill switch, but default to the first one
-rfkill="rfkill0"
-lsrfkill=""
-[ -e /sys/class/rfkill ] && lsrfkill=`/bin/ls /sys/class/rfkill/`
-for r in $lsrfkill; do
-    name=`cat /sys/class/rfkill/$r/name`
-    if [ "$name" == "eeepc-wlan" ]; then
-        msg="acpi-eeepc-generic-toggle-wifi.sh: Wifi rfkill switch find ($r)"
-        logger $msg
-        echo $msg
-        rfkill=$r
-    fi
-done
 
-RADIO_CONTROL_DEPRECATED=/proc/acpi/asus/wlan
-RADIO_CONTROL_OTHER=/sys/devices/platform/eeepc/wlan
-# Get rfkill switch state (0 = card off, 1 = card on)
-RADIO_CONTROL="/sys/class/rfkill/${rfkill}/state"
-if [ -e "$RADIO_CONTROL" ]; then
-    RADIO_STATE=$(cat $RADIO_CONTROL)
-else
-    RADIO_STATE=0
-    msg="acpi-eeepc-generic-toggle-wifi.sh: Wifi rfkill switch state does not exist. Using 0 as RADIO_STATE"
-    logger $msg
-    echo $msg
-fi
-
-# If the states has been saved, read that state. Else, get the
-# actual state.
-EEEPC_RADIO_SAVED_STATE_FILE=$EEEPC_VAR/states/wifi
-if [ -e $EEEPC_RADIO_SAVED_STATE_FILE ]; then
-  RADIO_SAVED_STATE=$(cat $EEEPC_RADIO_SAVED_STATE_FILE)
-else
-  RADIO_SAVED_STATE=$RADIO_STATE
-fi
+### Information for bluetooth ###################################
+DRIVER=$WIFI_DRIVER
+NAME="Wifi"
+NAME_SMALL="wlan"
+ICON="gnome-dev-wavelan"
+SYS_NAME="wlan"
+COMMANDS_PRE_UP="${COMMANDS_WIFI_PRE_UP[@]}"
+COMMANDS_PRE_DOWN="${COMMANDS_WIFI_PRE_DOWN[@]}"
+COMMANDS_POST_UP="${COMMANDS_WIFI_POST_UP[@]}"
+COMMANDS_POST_DOWN="${COMMANDS_WIFI_POST_DOWN[@]}"
+TOGGLE_MAX_TRY=${WIFI_TOGGLE_MAX_TRY}
 
 # Get wifi interface
-WIFI_IF=$(/usr/sbin/iwconfig 2>/dev/null | grep ESSID | awk '{print $1}')
+INTERFACE=$(/usr/sbin/iwconfig 2>/dev/null | grep ESSID | awk '{print $1}')
 
-function radio_toggle {
-    if [ "$RADIO_STATE" = "1" ]; then
-        radio_off 1
-    else
-        radio_on 1
-    fi
-}
+INTERFACE_UP="/sbin/ifconfig $INTERFACE up 2>/dev/null"
+INTERFACE_DOWN="/sbin/ifconfig $INTERFACE down 2>/dev/null"
 
-function radio_restore {
-  if [ "$RADIO_SAVED_STATE" = "1" ]; then
-    radio_on 1 0
-  else
-    radio_off 1 0
-  fi
-}
 
-function debug_wifi() {
-    print_generic_debug
-    print_generic_debug
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): Device: $RADIO_CONTROL_OTHER"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): Driver: $WIFI_DRIVER"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): Interface: $WIFI_IF"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): State: $RADIO_STATE"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): rfkill: $RADIO_CONTROL"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): COMMANDS_WIFI_PRE_UP:"
-    print_commands "${COMMANDS_WIFI_PRE_UP[@]}"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): COMMANDS_WIFI_POST_UP:"
-    print_commands "${COMMANDS_WIFI_POST_UP[@]}"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): COMMANDS_WIFI_PRE_DOWN:"
-    print_commands "${COMMANDS_WIFI_PRE_DOWN[@]}"
-    echo "DEBUG (acpi-eeepc-generic-toggle-wifi.sh): COMMANDS_WIFI_POST_DOWN:"
-    print_commands "${COMMANDS_WIFI_POST_DOWN[@]}"
+### Load saved state from file ##################################
+load_saved_state
 
-    eeepc_notify "Can you see this?" gtk-dialog-question 10000
+### Check rfkill switch #########################################
+check_rfkill_switch
 
-    eeepc_notify "Wifi
-Device: $RADIO_CONTROL_OTHER
-Driver: $WIFI_DRIVER
-Interface: $WIFI_IF
-State: $RADIO_STATE
-rfkill: $RADIO_CONTROL" gnome-dev-wavelan 10000
-}
+### Check /sys interface
+check_sys_interface
 
-function radio_on {
-    show_notifications=1
-    [ "$2" == "0" ] && show_notifications=
+### Detect if card is enabled or disabled #######################
+detect_if_enabled
 
-    [ "$RADIO_STATE" == "1" ] && eeepc_notify "Wifi already tuned on!" gnome-dev-wavelan && return 0
-
-    [ "$show_notifications" == "1" ] && \
-        eeepc_notify "Turning WiFi Radio on..." gnome-dev-wavelan
-
-    # Execute pre-up commands just once
-    [ $1 -eq 1 ] && execute_commands "${COMMANDS_WIFI_PRE_UP[@]}"
-
-    # Enable radio
-    [ -e "$RADIO_CONTROL" ]            && echo 1 > $RADIO_CONTROL
-    [ -e "$RADIO_CONTROL_DEPRECATED" ] && echo 1 > $RADIO_CONTROL_DEPRECATED
-    [ -e "$RADIO_CONTROL_OTHER" ]      && echo 1 > $RADIO_CONTROL_OTHER
-
-    # Load module
-    /sbin/modprobe $WIFI_DRIVER 2>/dev/null
-    success=$?
-    if [ $success ]; then
-        # If successful, enable card
-        echo 1 > $EEEPC_RADIO_SAVED_STATE_FILE
-        # Execute post-up commands
-        execute_commands "${COMMANDS_WIFI_POST_UP[@]}"
-
-        [ "$show_notifications" == "1" ] && eeepc_notify "WiFi Radio is now on" gnome-dev-wavelan
-    else
-        [ "$show_notifications" == "1" ] && eeepc_notify "Could not enable WiFi radio" stop
-        # If module loading unsuccessful, try again
-        if [ $1 -lt $WIFI_TOGGLE_MAX_TRY ]; then
-            [ "$show_notifications" == "1" ] && \
-                eeepc_notify "Trying again in 2 second ($(($1+1)) / $WIFI_TOGGLE_MAX_TRY)" gnome-dev-wavelan
-            sleep 2
-            radio_on $(($1+1)) $show_notifications
-        fi
-    fi
-}
-
-function radio_off {
-    show_notifications=1
-    [ "$2" == "0" ] && show_notifications=0
-
-    [ "$RADIO_STATE" == "0" ] && eeepc_notify "Wifi already tuned off!" gnome-dev-wavelan && return 0
-
-    [ "$show_notifications" == "1" ] && eeepc_notify "Turning WiFi Radio off..." gnome-dev-wavelan
-
-    # Execute pre-down commands just once
-    [ $1 -eq 1 ] && execute_commands "${COMMANDS_WIFI_PRE_DOWN[@]}"
-
-    # Put interface down and wait 1 second
-    /sbin/ifconfig $WIFI_IF down 2>/dev/null
-    sleep 1
-
-    # Unload module
-    /sbin/modprobe -r $WIFI_DRIVER 2>/dev/null
-    success=$?
-    if [ $success ]; then
-        # If successful, disable card through rkfill and save the state
-        [ -e "$RADIO_CONTROL" ]            && echo 0 > $RADIO_CONTROL
-        [ -e "$RADIO_CONTROL_DEPRECATED" ] && echo 0 > $RADIO_CONTROL_DEPRECATED
-        [ -e "$RADIO_CONTROL_OTHER" ]      && echo 0 > $RADIO_CONTROL_OTHER
-
-        echo 0 > $EEEPC_RADIO_SAVED_STATE_FILE
-
-        # Execute post-down commands
-        execute_commands "${COMMANDS_WIFI_POST_DOWN[@]}"
-
-        [ "$show_notifications" == "1" ] && eeepc_notify "WiFi Radio is now off" gnome-dev-wavelan
-    else
-        # If module unloading unsuccessful, try again
-        [ "$show_notifications" == "1" ] && eeepc_notify "Could not disable WiFi radio" stop
-        if [ $1 -lt $WIFI_TOGGLE_MAX_TRY ]; then
-                [ "$show_notifications" == "1" ] && \
-                    eeepc_notify "Trying again in 2 second ($(($1+1)) / $WIFI_TOGGLE_MAX_TRY)" gnome-dev-wavelan
-            sleep 2
-            radio_off $(($1+1)) $show_notifications
-        fi
-    fi
-}
-
+#################################################################
 case $1 in
-    restore)
-        radio_restore
+    "debug")
+        print_generic_debug
     ;;
-    stop|off)
-        radio_off 1
+    "restore")
+        device_restore
     ;;
-    start|on)
-        radio_on 1
+    "off")
+        device_off 1
     ;;
-    debug)
-        debug_wifi
+    "on")
+        device_on 1
     ;;
     *)
-        radio_toggle
+        device_toggle
     ;;
 esac
+
+### End of file #################################################
 
