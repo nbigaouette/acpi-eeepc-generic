048e2d183f36b51c83f839233d0b0fab916abf63
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-04-08 17:17:33 +0000

    Add madwifi quirks to toggle too

--- acpi-eeepc-generic-toggle-wifi.sh
+++ acpi-eeepc-generic-toggle-wifi.sh
@@ -2,17 +2,17 @@
 # Copyright 2009 Nicolas Bigaouette
 # This file is part of acpi-eeepc-generic.
 # http://code.google.com/p/acpi-eeepc-generic/
-# 
+#
 # acpi-eeepc-generic is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # acpi-eeepc-generic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with acpi-eeepc-generic.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -82,6 +82,16 @@ case $1 in
     ;;
     *)
         device_toggle
+        if [ "$WIFI_MADWIFI" == "yes" ]; then
+            # If the card was disabled, IS_ENABLED was set to "no". So
+            # toggling its status would enable it and we need to
+            # execute post_up quirks.
+            if [ "${IS_ENABLED}" == "no" ]; then
+                quirks_madwifi_post_up
+            else
+                quirks_madwifi_post_down
+            fi
+        fi
     ;;
 esac
 
