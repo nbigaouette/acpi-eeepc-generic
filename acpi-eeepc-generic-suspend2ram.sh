63012ac4da1d94498c904de71b0ed6a366fd98c3
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-10-06 19:01:30 +0000

    Fix issue #37: Autodetect vga and lvds in suspend when using SUSPEND_QUIRKS_VTSWITCH

--- acpi-eeepc-generic-suspend2ram.sh
+++ acpi-eeepc-generic-suspend2ram.sh
@@ -2,17 +2,17 @@
 # Copyright 2009 Nicolas Bigaouette
 # This file is part of acpi-eeepc-generic.
 # http://code.google.com/p/acpi-eeepc-generic/
-# 
+#
 # acpi-eeepc-generic is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # acpi-eeepc-generic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with acpi-eeepc-generic.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -68,7 +68,13 @@ if grep -q mem /sys/power/state ; then
         logger "Saving console number: $CONSOLE_NUMBER"
 
         # Turn off external monitor
-        xrandr --output LVDS --preferred --output VGA --off
+        var_xrandr="$EEEPC_VAR/xrandr.log"
+        xrandr > $var_xrandr
+        name_lvds=$(grep -i connected $var_xrandr | grep -i lvds | awk '{print ""$1""}')
+        if [ "x`grep " connected " $var_xrandr | awk '{print ""$1""}' | grep -i VGA`" != "x" ]; then
+            name_vga=$(grep -i connected $var_xrandr | grep -i vga | awk '{print ""$1""}')
+            xrandr --output ${name_lvds} --preferred --output ${name_vga} --off
+        fi
 
         # Change virtual terminal to not screw up X
         chvt 1
