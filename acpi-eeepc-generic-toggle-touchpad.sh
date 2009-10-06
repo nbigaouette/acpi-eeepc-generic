d1cc51255fa64c9b290e0891656d26df713d178a
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-10-06 16:39:31 +0000

    All synclient commands should use SHM mode

--- acpi-eeepc-generic-toggle-touchpad.sh
+++ acpi-eeepc-generic-toggle-touchpad.sh
@@ -2,17 +2,17 @@
 # Copyright 2009 Nicolas Bigaouette
 # This file is part of acpi-eeepc-generic.
 # http://code.google.com/p/acpi-eeepc-generic/
-# 
+#
 # acpi-eeepc-generic is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # acpi-eeepc-generic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with acpi-eeepc-generic.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -25,7 +25,7 @@ if [ -e "$STATE_FILE" ]; then
   TPSAVED=$(cat $STATE_FILE)
 fi
 
-enable=`synclient -l 2>&1`
+enable=`synclient -s -l 2>&1`
 if [ "$enable" == "Can't access shared memory area. SHMConfig disabled?" ]; then
     eeepc_notify "$enable" stop 10000
     eeepc_notify "Ensure xorg.conf is properly configured." stop 10000
@@ -34,10 +34,10 @@ fi
 
 function touchpad_toggle() {
     if [ -S /tmp/.X11-unix/X0 ]; then
-        TOUCHPAD_OFF=`synclient -l | grep TouchpadOff | awk '{print $3}'`
+        TOUCHPAD_OFF=`synclient -s -l | grep TouchpadOff | awk '{print $3}'`
         if [ "$TOUCHPAD_OFF" = "0" ]; then
             echo 0 > $STATE_FILE
-            synclient TouchpadOff=1
+            synclient -s TouchpadOff=1
             if [ $? ]; then
                 [ -e /usr/bin/unclutter ] && unclutter -idle 0 &
                 eeepc_notify "Touchpad Disabled" mouse
@@ -46,7 +46,7 @@ function touchpad_toggle() {
             fi
         else
             echo 1 > $STATE_FILE
-            synclient TouchpadOff=0
+            synclient -s TouchpadOff=0
             if [ $? ]; then
                 pkill unclutter
                 eeepc_notify "Touchpad Enabled" mouse
@@ -59,9 +59,9 @@ function touchpad_toggle() {
 
 function touchpad_restore() {
     if [ "$TPSAVED" = "0" ]; then
-        synclient TouchpadOff=1
+        synclient -s TouchpadOff=1
     else
-        synclient TouchpadOff=0
+        synclient -s TouchpadOff=0
     fi
 }
 
