1f89491d4c1dc6612d5c1b1ab56f34306d081b2e
nbigaouette <nbigaouette@3f9cffcc-e8cb-11dd-9ee4-63b70bd76fba>
2009-10-24 20:39:52 +0000

    Changed $EEEPC_USER3 to $EEEPC_SHE_TOGGLE

--- acpi-eeepc-generic-handler.sh
+++ acpi-eeepc-generic-handler.sh
@@ -2,17 +2,17 @@
 # Copyright 2009 Nicolas Bigaouette
 # This file is part of acpi-eeepc-generic.
 # http://code.google.com/p/acpi-eeepc-generic/
-# 
+#
 # acpi-eeepc-generic is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # acpi-eeepc-generic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with acpi-eeepc-generic.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -135,9 +135,13 @@ case "$1" in
                 logger "acpi-eeepc-generic-handler: (hotkey): Silver function button (User2)"
                 execute_commands "${COMMANDS_BUTTON_USER2[@]}"
             ;;
-            $EEEPC_USER3) # Fn+Space
-                logger "acpi-eeepc-generic-handler: (hotkey): Fn+Space"
-                execute_commands "${COMMANDS_BUTTON_USER3[@]}"
+#             $EEEPC_USER3) # Fn+Space
+#                 logger "acpi-eeepc-generic-handler: (hotkey): Fn+Space"
+#                 execute_commands "${COMMANDS_BUTTON_USER3[@]}"
+#             ;;
+            $EEEPC_SHE_TOGGLE) # Super Hybrid Engine
+                logger "acpi-eeepc-generic-handler: (hotkey): SHE"
+                execute_commands "${COMMANDS_SHE_TOGGLE[@]}"
             ;;
 
             $EEEPC_SLEEP)
